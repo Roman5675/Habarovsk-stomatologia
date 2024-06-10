073223ec0ccd4cd17d298f514ed53b322dce8047
Roman Pecherskikh <pwpec95@gmail.com>
2024-06-10 17:37:52 +0300

    10.06.2024

--- js/script.js
+++ js/script.js
@@ -1,39 +1,31 @@
-//Слайдер swiper
-new Swiper('.price-slider', {
-    slidesPerView: 3,
-    simulateTouch: true,
-    keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-    },
-    spaceBetween: 87,
-    loop: true,
-    loopedSlides: 0,
-    breakpoints: {
-        1400: {
-            slidesPerView: 3
-        },
-        993: {
-            slidesPerView: 2
-        },
-        0: {
-            slidesPerView: 1
-        },
+async function initMap() {
+    // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+    await ymaps3.ready;
 
-    },
-});
+    const {YMap, YMapDefaultSchemeLayer} = ymaps3;
 
-//Всплывающий header при scroll
-window.onscroll = function showHeader () {
+    // Иницилиазируем карту
+    const map = new YMap(
+        // Передаём ссылку на HTMLElement контейнера
+        document.getElementById('map'),
 
-    let header = document.querySelector('.header');
+        // Передаём параметры инициализации карты
+        {
+            location: {
+                // Координаты центра карты
+                center: [135.098564, 48.460415],
 
-    if(window.pageYOffset > 80) {
-        header.classList.add('header__fixed')
-    } else{
-        header.classList.remove('header__fixed')
-    }
+                // Уровень масштабирования
+                zoom: 18
+            }
+        }
+    );
+
+    // Добавляем слой для отображения схематической карты
+    map.addChild(new YMapDefaultSchemeLayer());
 }
+
+initMap();
 
 //Меню бургер
 const iconMenu = document.querySelector('.menu__icon');
@@ -44,73 +36,4 @@ if (iconMenu) {
         iconMenu.classList.toggle('_active');
         menuBody.classList.toggle('_active');
     });
-}
-
-//Прокрутка при клике на меню header
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-if(menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener("click", onMenuLinkClick);
-    });
-    function onMenuLinkClick(e) {
-        const menuLink = e.target;
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-            
-            if(iconMenu.classList.contains('_active')) {
-                document.body.classList.remove('_lock');
-                iconMenu.classList.remove('_active');
-                menuBody.classList.remove('_active');
-            }
-
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault();
-        }
-    }
-}
-
-//Прокрутка при клике на меню footer
-const footerLinks = document.querySelectorAll('.footer__link[data-goto]');
-if(footerLinks.length > 0) {
-    footerLinks.forEach(footerLink => {
-        footerLink.addEventListener("click", onFooterLinkClick);
-    });
-    function onFooterLinkClick(e) {
-        const footerLink = e.target;
-        if (footerLink.dataset.goto && document.querySelector(footerLink.dataset.goto)) {
-            const gotoBlock = document.querySelector(footerLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-            
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault();
-        }
-    }
-}
-
-//Прокрутка при клике на Sign up
-const signUp = document.querySelectorAll('.intro__link[data-goto]');
-if(signUp.length > 0) {
-    signUp.forEach(signUp => {
-        signUp.addEventListener("click", onSignUpLinkClick);
-    });
-    function onSignUpLinkClick(e) {
-        const signUp = e.target;
-        if (signUp.dataset.goto && document.querySelector(signUp.dataset.goto)) {
-            const gotoBlock = document.querySelector(signUp.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-            
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault();
-        }
-    }
 }
