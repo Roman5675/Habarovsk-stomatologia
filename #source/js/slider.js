f4f93cd7aa61823e8b76a3a50c0568b5de68d21d
Roman Pecherskikh <pwpec95@gmail.com>
2024-07-25 16:32:10 +0300

    25.07.2024

--- #source/js/slider.js
+++ #source/js/slider.js
@@ -1,27 +1,67 @@
-new Swiper('.price-slider', {
+const swiper = new Swiper('.price-slider', {
     slidesPerView: 5,
     centeredSlides: true,
-    simulateTouch: true,
+    touchEventsTarget: 'swiper-wrapper',
+    grabCursor: true,
     keyboard: {
         enabled: true,
         onlyInViewport: true,
     },
     spaceBetween: 0,
     loop: true,
+    speed: 600, // Устанавливаем скорость перехода в миллисекундах
     breakpoints: {
-        1400: {
+        701: {
             slidesPerView: 5
         },
-        993: {
-            slidesPerView: 2
-        },
         0: {
-            slidesPerView: 1
+            slidesPerView: 3
         },
-
     },
     pagination: {
         el: '.swiper-pagination',
-        clickable: true, // Сделать точки кликабельными
+        clickable: true,
     },
+    on: {
+        init: function() {
+            updateSlideWidths(this);
+        },
+        slideChangeTransitionStart: function() {
+            updateSlideWidths(this);
+        },
+        slideChangeTransitionEnd: function() {
+            updateSlideWidths(this);
+        }
+    }
+});
+
+function updateSlideWidths(swiper) {
+    var slides = swiper.slides;
+    var activeIndex = swiper.activeIndex;
+    var nextIndex = activeIndex + 1 < slides.length ? activeIndex + 1 : 0;
+    var prevIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : slides.length - 1;
+
+    slides.forEach(function(slide, index) {
+        var scale = 0.41;
+        var zIndex = 1;
+        if (index === activeIndex) {
+            scale = 1;
+            zIndex = 3;
+        } else if (index === nextIndex || index === prevIndex) {
+            scale = 0.54;
+            zIndex = 2;
+        }
+        slide.style.transform = `scale(${scale})`;
+        slide.style.zIndex = zIndex;
+
+        var image = slide.querySelector('img');
+        if (image) {
+            var imgWidth = image.naturalWidth * scale;
+            slide.style.width = `${imgWidth}px`;
+        }
+    });
+}
+
+window.addEventListener('load', function() {
+    updateSlideWidths(swiper);
 });
